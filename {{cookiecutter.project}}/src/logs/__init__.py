2db1e145a541e5756baada9b1d3f7ac3997772bf
Sankha Subhra Mukherjee <sankha.mukherjee@gmail.com>
2017-10-27 18:51:41 +0800

    changes to the module description

--- {{cookiecutter.project}}/src/logs/__init__.py
+++ {{cookiecutter.project}}/src/logs/__init__.py
@@ -1,8 +1,7 @@
 '''module containing helper functions for logging
 
-This module just contains a couple of decorators. This
-is useful when you want to wrap your function into
-a decorator. The decorators will automatically inject 
-the right decorator into your module withoput you having
-to worry about the decorator. 
+This module contains two functions. The first one 
+will be used for generating a new logger object, 
+and another one for uisng that logging object
+for new tasks. 
 '''

976f65d780e94e6dd6634232fad84a1ff49cf8b6
Sankha Subhra Mukherjee <sankha.mukherjee@gmail.com>
2018-11-23 12:55:29 +0800

    master (feat): Add some high performance database libraries
     Changes to be committed:
    	modified:   {{cookiecutter.project}}/config/db.template.json
    	new file:   {{cookiecutter.project}}/src/lib/databaseIO/__init__.py
    	new file:   {{cookiecutter.project}}/src/lib/databaseIO/pgIO.py
    	modified:   {{cookiecutter.project}}/src/lib/testLib/__init__.py

--- {{cookiecutter.project}}/src/lib/testLib/__init__.py
+++ {{cookiecutter.project}}/src/lib/testLib/__init__.py
@@ -3,5 +3,5 @@
 Dont use this folder. This has been placed here solely for demonstration. 
 You are welcome to use this folder as a template for other libraries. This
 folder has a single file, and within that a single folder, for demo 
-purposes.
+purposes. It is possible that other libraries will be added as necessary.
 """
